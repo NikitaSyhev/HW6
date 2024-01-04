338f2b20836e9ca8163024893e5b89525ec7dc89
Nikita Sychev <nikitasychev@inbox.ru>
2024-01-04 14:17:45 +0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+
+function Vehicle(name, speed, x, y) {
+    if(!new.target) { 
+        throw new Error('Invalid inicialization: operator new expected;'); //
+    }
+    this.name = name;
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
+    this.move = function(x,y) {
+        this.x = x;
+        this.y = y;
+    } 
+}
+
+function Car(name, speed, x,y, horsePower) {
+    this.horsePower = horsePower;
+    this.__proto__= new Vehicle(name, speed, x, y); 
+    this.beep = function() {
+        console.log('Биииип');
+    }
+}
+
+
+//создаем объект CAR
+let car1 = new Car('BMW', 200, 20, 20, 300);
+// выводим его в консоль
+console.log(car1);
+console.log('Вызываем метод beep().');
+car1.beep();
+car1.move(50,50);
+console.log(car1);
